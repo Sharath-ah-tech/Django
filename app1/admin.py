889965c54b04777d0a83ad99b7a93c0bbd055b19
Sharath-ah-tech <sharath3012005@gmail.com>
2026-04-20 22:44:23 +0530

    Added admin

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Employee, Task, Project, Order, Certificate
 # Register your models here.
+admin.site.register(Employee)
+admin.site.register(Task)
+admin.site.register(Project)
+admin.site.register(Order)
+admin.site.register(Certificate)
